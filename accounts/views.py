0356d1f0796ab902773c03338e57c6864ce72846
Bishnu <thapabishnu20@gmail.com>
2023-03-16 16:37:48 +0545

    user registration views added

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        messages.success(request, 'User created Successfully')
+        return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form': form})
